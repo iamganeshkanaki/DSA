715d24c259b422e7bab01dc214993f8b8aba414a
Ganesh <mc22029.@iims.ac.in>
2024-01-02 17:18:50 +0530

    done

--- Array and Hashing/simple5.js
+++ Array and Hashing/simple5.js
@@ -1,28 +1,13 @@
-"use strickt";
-const input = require("prompt-sync")();
-let arr = [];
-let l = parseInt(input("Enter a Length of Array: "));
-for (let i = 0; i < l; i++) {
-    arr.push(parseInt(input("Enter a Element of Array: ")));
+arr = [5,3,1,2,4];
+
+
+let temp = 0;
+for(let i = 0; i <= arr.length; i++){    
+    for(let j = 0; j <= arr.length; j++){
+        if(arr[i] > arr[j + 1]){
+            temp = arr[i];
+        }
+    }
+    arr[i] = temp;
 }
-let sdlgn = arr[0];
-let lgn = arr[0];
-let sml = arr[0];
-
-for (let j = 0; j < l; j++) {
-    if (lgn <= arr[j + 1]) {
-        lgn = arr[j + 1];
-    }
-    if(sml >= arr[j + 1]){        
-        sml = arr[j + 1];
-    }
-
-    if(arr[j] > sml && arr[j] < lgn){
-        sdlgn = arr[j];
-    }
-}
-console.log(lgn);
-console.log(sml);
-console.log(sdlgn);
-
-
+console.log(arr)
