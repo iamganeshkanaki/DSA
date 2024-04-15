ed871789c8ff42fdb0c3668191e42565354a35a0
Ganesh <mc22029.@iims.ac.in>
2024-04-15 17:43:01 +0530

    3 sorting done

--- Sorting/Insertion Sort/page.js
+++ Sorting/Insertion Sort/page.js
@@ -4,15 +4,15 @@ let n = parseInt(input("enter a Size: "));
 for (let i = 0; i < n; i++) {
     arr.push(parseInt(input("enter a elements:")));    
 }
-for (let i = 0; i < arr.length; i++) {
-    let min = i;
+
+for (let i = 1; i < arr.length; i++) {    
     for (let j = 0; j < arr.length; j++) {
-        if (arr[j] > arr[min]){
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        if (arr[j] > arr[i]) {
+            let temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
         }
     }
 }
 
-console.log(arr)
+console.log(arr);
